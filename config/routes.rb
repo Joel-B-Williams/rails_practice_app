8eb7e3209768ee17396442f435cbbe7dd43e8c73
Joel-B-Williams <joel.b.williams.is@gmail.com>
2017-03-27 12:05:47 -0500

    basic user profile

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   get '/signup', to: 'users#new'
-
+  resources :users
   # get 'static_pages/home'
 	get '/help', to: 'static_pages#help'
 	get '/about', to: 'static_pages#about'
