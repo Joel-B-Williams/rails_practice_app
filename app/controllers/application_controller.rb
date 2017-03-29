53dcf7dbdf25229c36023460de9b86f48d86c633
Joel-B-Williams <joel.b.williams.is@gmail.com>
2017-03-29 15:39:00 -0500

    session helper methods & link to all controllers

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,7 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
-
+  # helpers for sessions will now be available in all controllers
+  include SessionsHelper
   # def hello
   # 	render html: "Hello, World!"
   # end
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,6 +6,8 @@ class SessionsController < ApplicationController
   	user = User.find_by(email: params[:session][:email].downcase)
   	if user && user.authenticate(params[:session][:password])
   		#log in
+  		log_in(user)
+  		redirect_to user
   	else
   		#flash.now will not carry until the next full request.  a render (as below) is not a "request" so the standard flash message carries through to the next link you visit.
 			flash.now[:danger] = "Incorrect username/password combination"
