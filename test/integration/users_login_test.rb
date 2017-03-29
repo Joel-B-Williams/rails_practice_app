4728a824c1dcaabdea7caa0c90a130fd9a1a1d55
Joel-B-Williams <joel.b.williams.is@gmail.com>
2017-03-29 17:01:13 -0500

    integration tests for login checking proper links shown

--- test/integration/users_login_test.rb
+++ test/integration/users_login_test.rb
@@ -5,6 +5,11 @@ class UsersLoginTest < ActionDispatch::IntegrationTest
   #   assert true
   # end
 
+  def setup
+    #users = users.yml, :cat = key to cat hash
+    @user = users(:cat)
+  end
+  
   test "gets login path" do 
   	get login_path
   	assert_template 'sessions/new'
@@ -13,5 +18,17 @@ class UsersLoginTest < ActionDispatch::IntegrationTest
   	assert_not flash.empty?
   	get root_path
   	assert flash.empty?
+  end
+
+  test "appropriate links are shown" do 
+    get login_path
+    #password hardcoded here because there's no password column in DB so rails would freak if it was in yml file
+    post login_path, params: { session: { email: @user.email, password: 'password' }}
+    assert_redirected_to @user
+    follow_redirect!
+    assert_template 'users/show'
+    assert_select "a[href=?]", login_path, count: 0
+    assert_select "a[href=?]", logout_path 
+    assert_select "a[href=?]", user_path(@user) 
   end
 end
