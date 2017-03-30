e4dbee0517f4a3c4dc3253c3505591d9b3da69a7
Joel-B-Williams <joel.b.williams.is@gmail.com>
2017-03-30 13:41:39 -0500

    method to authenticate remember_token

--- app/models/user.rb
+++ app/models/user.rb
@@ -34,4 +34,10 @@ class User < ApplicationRecord
     # Note skipping validations with update_attr is good because we don't have the instance's password/confirmation to verify
     update_attribute(:remember_digest, User.digest(remember_token))
   end
+
+# Remember back to writing BCrypt password authentication -> this is doing the same with the hashed remember_token (cookie).  Reinflate the string held in DB, use re-written == to hash the virtual remember_token and compare the two.
+# Same process, same reason - keeps cookie encrypted so even if it gets stolen the damage potential is minimalized.
+  def authenticated?(remember_token)
+    BCrypt::Password.new(remember_digest) == remember_token
+  end
 end
