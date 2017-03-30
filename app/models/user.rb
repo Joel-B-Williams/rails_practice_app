038245e7bf3be5b74c14f844b3606562d3a8a6e0
Joel-B-Williams <joel.b.williams.is@gmail.com>
2017-03-30 11:50:22 -0500

    Remember-token as VirtAttr, DB hash, plus associated methods

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,7 @@
 class User < ApplicationRecord
+  #create an accessible token 
+  attr_accessor :remember_token
+
 	before_save { email.downcase! }
 	validates :name, presence: true, length: {maximum: 50}
 	VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
@@ -15,5 +18,17 @@ class User < ApplicationRecord
                                                   BCrypt::Engine.cost
     BCrypt::Password.create(string, cost: cost)
   end
+  # creates random 22-char string, will be using for access token key -> will be encrypted similarly to password digest
+  def User.new_token
+    SecureRandom.urlsafe_base64
+  end
 
+# method to associate a 'remember token' with user and store it in DB
+  def User.remember
+    # virtual attribute becomes a random string
+    self.remember_token = User.new_token
+    # DB col becomes hashed version of that string (similar to password)
+    # Note skipping validations with update_attr is good because we don't have the instance's password/confirmation to verify
+    update_attribute(:remember_digest, User.digest(remember_token))
+  end
 end
