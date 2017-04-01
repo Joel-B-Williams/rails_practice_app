8d63dc87af91009cadfdcb76e1e2ca2339f16ff9
Joel-B-Williams <joel.b.williams.is@gmail.com>
2017-04-01 10:37:23 -0500

    update dev config file for mailer specs

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -27,8 +27,16 @@ Rails.application.configure do
   end
 
   # Don't care if the mailer can't send.
+
+  # UPDATES HERE
+  # set delivery errors to true
+  # set delivery method to :test
+  # set host to localhost (or cloud host if using)
+  # set url options to host & http protocol
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :test
+  host = 'localhost:3000'
+  config.action_mailer.default_url_options = { host: host, protocol: 'http'}
   
   config.action_mailer.perform_caching = false
 
