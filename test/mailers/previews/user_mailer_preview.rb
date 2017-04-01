e94a76433eb8c0029e09a39ff254c36ed47a6ab5
Joel-B-Williams <joel.b.williams.is@gmail.com>
2017-04-01 10:45:54 -0500

    edit mailer views to use _url + notes

--- test/mailers/previews/user_mailer_preview.rb
+++ test/mailers/previews/user_mailer_preview.rb
@@ -2,8 +2,15 @@
 class UserMailerPreview < ActionMailer::Preview
 
   # Preview this email at http://localhost:3000/rails/mailers/user_mailer/account_activation
+
+  # calls method in user_mailer.rb file
   def account_activation
-    UserMailer.account_activation
+  	#define a user since method requires one now
+    user = User.first
+    # set its activation token as the DB doesn't have it - it's virtual
+    user.activation_token = User.new_token
+    # call method on our user
+    UserMailer.account_activation(user)
   end
 
   # Preview this email at http://localhost:3000/rails/mailers/user_mailer/password_reset
