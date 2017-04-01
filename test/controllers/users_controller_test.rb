395c2020b3ba0e6a370f5286865fad10e409e418
Joel-B-Williams <joel.b.williams.is@gmail.com>
2017-03-31 19:45:00 -0500

    put index int tests in correct place

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -44,26 +44,4 @@ class UsersControllerTest < ActionDispatch::IntegrationTest
     assert_redirected_to login_path
   end
 
-  test "should redirect destroy when not logged in" do 
-    assert_no_difference 'User.count' do 
-      delete user_path(@user)
-    end
-    assert_redirected_to login_path
-  end
-
-  test "should redirect destroy when logged in as non-admin" do 
-    log_in_as(@other_user)
-    assert_no_difference 'User.count' do 
-      delete user_path(@user)
-    end
-    assert_redirected_to root_path
-  end
-
-  test "admins can activate orbital railguns" do 
-    log_in_as(@user)
-    assert_difference 'User.count', -1 do 
-      delete user_path(@other_user)
-    end
-  end
-
 end
--- test/integration/users_index_test.rb
+++ test/integration/users_index_test.rb
@@ -6,6 +6,7 @@ class UsersIndexTest < ActionDispatch::IntegrationTest
   # end
   def setup
   	@user = users(:cat)
+    @other_user = users(:archer)
 	end
 
   test "index including pagination" do 
@@ -16,5 +17,28 @@ class UsersIndexTest < ActionDispatch::IntegrationTest
   	User.paginate(page: 1).each do |user|
   		assert_select 'a[href=?]', user_path(user), text: user.name
   	end
+  end
+
+
+  test "should redirect destroy when not logged in" do 
+    assert_no_difference 'User.count' do 
+      delete user_path(@user)
+    end
+    assert_redirected_to login_path
+  end
+
+  test "should redirect destroy when logged in as non-admin" do 
+    log_in_as(@other_user)
+    assert_no_difference 'User.count' do 
+      delete user_path(@user)
+    end
+    assert_redirected_to root_path
+  end
+
+  test "admins can activate orbital railguns" do 
+    log_in_as(@user)
+    assert_difference 'User.count', -1 do 
+      delete user_path(@other_user)
+    end
   end
 end
