df9aa235c6c59da58bce6d215070bfbbfdf98e46
Joel-B-Williams <joel.b.williams.is@gmail.com>
2017-04-01 11:21:46 -0500

    adjust generated test for mailer - account activation

--- test/mailers/user_mailer_test.rb
+++ test/mailers/user_mailer_test.rb
@@ -2,11 +2,18 @@ require 'test_helper'
 
 class UserMailerTest < ActionMailer::TestCase
   test "account_activation" do
-    mail = UserMailer.account_activation
+    user = users(:cat)
+    user.activation_token = User.new_token
+    mail = UserMailer.account_activation(user)
     assert_equal "Account activation", mail.subject
-    assert_equal ["to@example.org"], mail.to
-    assert_equal ["from@example.com"], mail.from
-    assert_match "Hi", mail.body.encoded
+    assert_equal [user.email], mail.to
+    # assert_equal ["to@example.org"], mail.to
+    assert_equal ["noreply@example.com"], mail.from
+    # assert_equal ["from@example.com"], mail.from
+    # make sure user name, token, and escaped email are in the email body
+    assert_match user.name, mail.body.encoded
+    assert_match user.activation_token, mail.body.encoded
+    assert_match CGI.escape(user.email), mail.body.encoded
   end
 
   test "password_reset" do
